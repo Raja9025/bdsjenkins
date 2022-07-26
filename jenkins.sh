421f7429e688469ff70afb78d7a564dfc9ee35a1
raja9025 <S.raja@banyancloud.io>
2022-07-26 12:04:21 +0530

    commit-8

--- jenkins.sh
+++ jenkins.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -x
 deployutil=deployfile/deployutil
+services=( 'aws-collector' 'dtcs-mysql' 'dtcscol-mysql' 'dtcs-pgsql' 'dtcscol-pgsql' 'dtcs-mssql' 'dtcscol-mssql' 'encs' 'gcp-collector' 'ims' 'lmos-aws' 'lmos-gcp' 'lmos-oci' 'oci-collector' 'recs-aws' 'recs-gcp' 'recs-oci' 'scos' )
 
-for deploy in "$(cat microservices.txt)"
+for deploy in "${services[@]}"
 do
     #${deployutil} -m ${deploy} int
-     echo $deploy
+     echo ${deploy}
 done
 
-for image in "$(cat microservices.txt)"
+for image in "${services[@]}"
 do
     echo "The Current Images Of Microservice ${image}"
     ${deployutil} -c ${image}
