fca01d62a77eabd587624c7a63d8f722d7f7ecd9
raja9025 <S.raja@banyancloud.io>
2022-07-26 11:39:31 +0530

    commit-3

--- jenkins.sh
+++ jenkins.sh
@@ -6,13 +6,11 @@ for deploy in "$(cat microservices.txt)"
 do
     #${deployutil} -m ${deploy} int
      echo $deploy
-   echo "---------------------------------------------------"
 done
 
 for image in "$(cat microservices.txt)"
 do
     echo "The Current Images Of Microservice ${image} are:"
     ${deployutil} -c ${image}
-    echo "==================================================="
 done
 
